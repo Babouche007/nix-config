42ec46a3dbe6c360addbba8586203a9c54d85354
Baptiste Perrier <baptiste.perrier@epita.fr>
2022-11-22 08:50:43 +0100

    merged init.sh

--- init.sh
+++ init.sh
@@ -3,14 +3,9 @@ echo "Creating / updating needed channel and preparing nvim config"
 nix-channel --add https://nixos.org/channels/nixos-22.05 nixos-stable
 nix-channel --update
 
-<<<<<<< HEAD
-nix-env -iA nixos-unstable.home-manager
-
-=======
 nix-env -iA nixos-stable.home-manager
 
 rm -rf ~/.config/nixpkgs
->>>>>>> 51815ef (fixing issues due to the use of unstable channel)
 mkdir -p ~/.config/nixpkgs
 cp -r ./nixpkgs/* ~/.config/nixpkgs/
 
