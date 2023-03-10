0841660ded2fc9da0f335a3d612187aa7a39769a
Babouche <bat.perrier@free.fr>
2023-03-10 12:28:11 +0100

    [Removing] polybar configuration

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -9,6 +9,5 @@
 	programs.zsh = import ./zsh/zsh.nix {};
 	services.picom = import ./picom/picom.nix {};
 	programs.alacritty = import ./alacritty/alacritty.nix pkgs;
-	services.polybar = import ./polybar/polybar.nix pkgs;
 
 }
